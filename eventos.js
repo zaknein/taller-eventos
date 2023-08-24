fa08a994b6dd8c97734fea63e71f033fd19b9335
German Kröger <gkroger11@gmail.com>
2023-08-24 17:14:57 -0300

    finalizado

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let divElement = document.querySelector("div");
+
+    divElement.addEventListener("click", function(){
+        alert("Hola soy un div");
+    });
+});
